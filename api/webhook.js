13227a0d0b090bebc71df80c7c5be77dbe0ba091
jhiwane <christinawiltonn@gmail.com>
2025-12-25 21:08:20 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,6 @@
 const bot = require('../lib/botConfig');
 
 module.exports = async (req, res) => {
-    // 1. Setup CORS standar Vercel
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
@@ -9,13 +8,10 @@ module.exports = async (req, res) => {
     if (req.method === 'OPTIONS') return res.status(200).end();
 
     try {
-        // 2. Oper data dari Telegram ke Bot Config
-        if (req.body) {
-            await bot.handleUpdate(req.body);
-        }
+        if (req.body) await bot.handleUpdate(req.body);
         res.status(200).json({ ok: true });
     } catch (e) {
-        console.error("Webhook Error:", e);
+        console.error(e);
         res.status(500).json({ error: e.message });
     }
 };
